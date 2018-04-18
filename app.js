6b53484d2f52b802eea1b8f31a0baac282506560
codi83 <codi83@gmail.com>
2018-04-18 17:07:43 +0900

    게시판기본

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//import modules
 var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-mongoose.connect(process.env.MONGO_DB); 
+//connect db
+mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open", function () {
   console.log("DB connected!!!");
@@ -12,23 +16,87 @@ db.on("error",function () {
   console.log("DB ERROR : ", err);
 });
 
-var dataSchema = mongoose.Schema({
-  name:String,
-  count:Number
+//model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt:Date
 });
-var Data = mongoose.model('data', dataSchema);
-Data.findOne({name:"myData"},function (err,data) {
-  if(err) return console.log("data ERROR!:", err);
-  if(!data){
-    Data.create({name:"myData",count:0},function (err,data) {
-      if(err) return console.log("DATA ERROR : ", err);
-      console.log("Counter initialized : ", data);
-    });
-  }
-});
+var Post = mongoose.model('post',postSchema);
 
+//view setting
 app.set("view engine", 'ejs');
+
+//set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+//set routes
+app.get('/posts', function (req,res) {
+  Post.find({}).sort('-createdAt').exec(function(err,posts){
+    if(err) return res.json({success:false, message:err});
+    //res.json({success:true, data:posts});
+    res.render("posts/index", {data:posts});
+  });
+});//index
+app.get('/posts/new', function(req,res){
+  res.render("posts/new");
+});//new
+app.post('/posts',function(req,res){
+  console.log(req.body);
+  Post.create(req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, data:post});
+    res.redirect('/posts');
+  });
+});//create
+app.get('/posts/:id', function(req,res){
+  Post.findById(req.params.id, function(err,post) {
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, data:post});
+    res.render("posts/show",{data:post});
+  });
+});//show
+app.get('/posts/:id/edit', function(req,res){
+  Post.findById(req.params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/edit", {data:post});
+  });
+});//edit
+app.put('/posts/:id', function(req,res){
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, message:post._id+" updated"});
+    res.redirect('/posts/'+req.params.id);
+  });
+});//update
+app.delete('/posts/:id', function(req,res){
+  Post.findByIdAndRemove(req.params.id, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    // res.json({success:true, message:post._id+" deleted"});
+    res.redirect('/posts');
+  });
+});//destroy
+// var dataSchema = mongoose.Schema({
+//   name:String,
+//   count:Number
+// });
+// var Data = mongoose.model('data', dataSchema);
+// Data.findOne({name:"myData"},function (err,data) {
+//   if(err) return console.log("data ERROR!:", err);
+//   if(!data){
+//     Data.create({name:"myData",count:0},function (err,data) {
+//       if(err) return console.log("DATA ERROR : ", err);
+//       console.log("Counter initialized : ", data);
+//     });
+//   }
+// });
+
+
 
 app.get('/', function (req,res) {
   Data.findOne({name:"myData"},function (err,data) {
